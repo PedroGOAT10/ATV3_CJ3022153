8e8d58a90d955407c38c9e2a630c54ee3e8d18f5
Pedro Henrique <santos.pedro3@aluno.ifsp.edu.br>
2024-03-06 09:09:37 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+            float n;
+
+            
+            Console.WriteLine("Digite a sua nota: ");
+            
+            n = float.Parse(Console.ReadLine());
+
+            if (n >= 6) {
+
+                Console.WriteLine("Aluno foi Aprovado"); 
+            }
+            else{  Console.WriteLine("Aluno foi Reprovado");
+            }
+        
+    
+
+  
